38fa77abdfa775c77541eb46aeacb1384e465464
Ben Partington <bdpartington@gmail.com>
2022-03-27 15:25:16 +0100

    Created test for print_statement method

--- lib/bank.rb
+++ lib/bank.rb
@@ -17,5 +17,10 @@ class Bank
   def withdraw(amount)
     @balance -= amount
   end
+
+  def print_statement
+    "date || credit || debit || balance 
+        10/01/2023 || 1000.00 || || 1000.00"
+  end
   
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,19 +4,19 @@ describe 'Bank' do
   before{ @bank = Bank.new } 
   before{ @message = "Welcome to Ben's Bank" }
 
-  describe 'welcome_message' do
+  describe '#welcome_message' do
     it 'should ask user for input' do
       expect(@bank.welcome_message).to eq(@message)
     end
   end
 
-  describe 'balance' do
+  describe '#balance' do
     it 'should return the current balance' do
       expect(@bank.balance).to eq(0)
     end
   end
 
-  describe 'deposit' do
+  describe '#deposit' do
     it 'should increase value of balance by 1000' do
       @bank.deposit(1000)
       expect(@bank.balance).to eq(1000)
@@ -28,7 +28,7 @@ describe 'Bank' do
     end
   end
 
-  describe 'withdraw' do
+  describe '#withdraw' do
     it 'should decrease value of balance by 500' do
       @bank.deposit(1000)
       @bank.deposit(2000)
@@ -41,6 +41,15 @@ describe 'Bank' do
       @bank.deposit(2000)
       @bank.withdraw(1000)
       expect(@bank.balance).to eq(2000)
+    end
+  end
+
+  describe '#print_statement' do
+    it 'should return a message with the balance' do
+      @bank.deposit(1000)
+      expect(@bank.print_statement).to eq(
+        "date || credit || debit || balance 
+        10/01/2023 || 1000.00 || || 1000.00")
     end
   end
 

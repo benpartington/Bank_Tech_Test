7a5380d3d3854750473c822c63646299d011e86e
Ben Partington <bdpartington@gmail.com>
2022-03-28 15:45:27 +0100

    Refactored to use a history array of strings, which is populated by deposit and withdraw methods

--- lib/bank.rb
+++ lib/bank.rb
@@ -5,11 +5,7 @@ class Bank
   def initialize(welcome_class = nil) #dependency injection
     @balance = 0
     @instance_of_welcome_class = welcome_class
-    @history = { } #add dates, transactions and balance to this hash?
-  end
-
-  def history
-    @history
+    @history = ["date || credit || debit || balance\n"]
   end
 
   def welcome
@@ -19,18 +15,17 @@ class Bank
   def deposit(amount)
     @balance += amount
     @credit = amount
-    @history[:credit] = amount # add credit transaction amount to hash
+    @history << "10/01/2023 || #{@credit} || || #{@balance}"
   end
 
   def withdraw(amount)
     @balance -= amount
     @debit = amount
-    @history[:debit] = amount # add debit transaction amount to hash
+    @history << "10/01/2023 || || #{@debit} || #{@balance}"
   end
 
   def print_statement
-    "date || credit || debit || balance 
-        10/01/2023 || #{@credit} || #{@debit} || #{@balance}"
+    @history   
   end
   
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -40,16 +40,13 @@ describe 'Bank' do
   describe '#print_statement' do
     it 'should return message with correct credit and balance when balance increased by 1000' do
       @bank.deposit(1000)
-      expect(@bank.print_statement).to eq(
-        "date || credit || debit || balance 
-        10/01/2023 || 1000 ||  || 1000")
+      expect(@bank.print_statement).to eq(["date || credit || debit || balance\n", "10/01/2023 || 1000 || || 1000"])
     end
+  
 
     it 'should return message with correct credit and balance when balance increased by 2000' do
       @bank.deposit(2000)
-      expect(@bank.print_statement).to eq(
-        "date || credit || debit || balance 
-        10/01/2023 || 2000 ||  || 2000")
+      expect(@bank.print_statement).to eq(["date || credit || debit || balance\n", "10/01/2023 || 2000 || || 2000"])
     end
 
     it 'should return message with correct credit, debit and balance when balance decreased by 500' do
